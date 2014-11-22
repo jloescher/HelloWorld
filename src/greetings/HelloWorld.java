8bfed3519fe5e2eb1ed1f06dd57158803b580d25
Jonathan Loescher <jonathan@jloescher.com>
2014-11-22 12:21:18 -0500

    Initial commit

---
+++ src/greetings/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Jonathan on 11/22/2014.
+ */
+package greetings;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int number = 3;
+        String name = "Jonathan";
+
+        name ="Kevin";
+
+        number = 5 + 2;
+
+        System.out.println(number);
+        System.out.println(name);
+    }
+}
